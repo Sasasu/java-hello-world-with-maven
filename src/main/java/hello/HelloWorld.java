90ace4cfb46a80af60f9d30fd76131c79f7e174b
Sasasu <i@sasa.su>
2023-03-15 22:23:20 +0800

    pdf ocr demo

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,13 +1,41 @@
 package hello;
 
-import org.joda.time.LocalTime;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.pdf.PDFParser;
+import org.apache.tika.sax.BodyContentHandler;
+
+// https://www.tutorialspoint.com/tika/tika_extracting_pdf.htm
+
+import org.xml.sax.SAXException;
 
 public class HelloWorld {
-    public static void main(String[] args) {
-      LocalTime currentTime = new LocalTime();
-		  System.out.println("The current local time is: " + currentTime);
 
-        Greeter greeter = new Greeter();
-        System.out.println(greeter.sayHello());
-    }
+   public static void main(final String[] args) throws IOException,TikaException, SAXException {
+
+      BodyContentHandler handler = new BodyContentHandler();
+      Metadata metadata = new Metadata();
+      FileInputStream inputstream = new FileInputStream(new File(args[0]));
+      ParseContext pcontext = new ParseContext();
+      
+      //parsing the document using PDF parser
+      PDFParser pdfparser = new PDFParser();
+      pdfparser.parse(inputstream, handler, metadata,pcontext);
+      
+      //getting the content of the document
+      System.out.println("Contents of the PDF :" + handler.toString());
+      
+      //getting metadata of the document
+      System.out.println("Metadata of the PDF:");
+      String[] metadataNames = metadata.names();
+      
+      for(String name : metadataNames) {
+         System.out.println(name+ " : " + metadata.get(name));
+      }
+   }
 }
